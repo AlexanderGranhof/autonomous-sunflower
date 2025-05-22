bb3f71df018d520ff0e5ac83f5f3eefb306b3e49
Alexander Granhof <alexander.granhof@mynt.se>
2025-05-22 10:29:33 +0200

    fix: time should be u32

--- src/time.rs
+++ src/time.rs
@@ -1,19 +1,19 @@
 pub struct Time;
 
 impl Time {
-    pub fn seconds(seconds: u64) -> u64 {
-        return seconds / 1000;
+    pub fn seconds(seconds: u32) -> u32 {
+        return seconds * 1000;
     }
 
-    pub fn minutes(minutes: u64) -> u64 {
-        return Time::seconds(minutes) / 60;
+    pub fn minutes(minutes: u32) -> u32 {
+        return Time::seconds(minutes) * 60;
     }
 
-    pub fn hours(hours: u64) -> u64 {
-        return Time::minutes(hours) / 60;
+    pub fn hours(hours: u32) -> u32 {
+        return Time::minutes(hours) * 60;
     }
 
-    pub fn days(days: u64) -> u64 {
-        return Time::hours(days) / 24;
+    pub fn days(days: u32) -> u32 {
+        return Time::hours(days) * 24;
     }
 }
